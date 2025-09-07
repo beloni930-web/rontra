c44238c8177f0d5558585e0d85965dc1928ca95a
beloni930-web <beloni930@gmail.com>
2025-09-07 16:07:17 +0900

    Add files via upload

---
+++ rontra/assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAXigmZU5RDVU60fACbD5t9GHt4JwQzzBg",
+  authDomain: "encdst.firebaseapp.com",
+  projectId: "encdst",
+  storageBucket: "encdst.firebasestorage.app",
+  messagingSenderId: "668627014363",
+  appId: "1:668627014363:web:41b301c5a3c5697b95b2fe",
+  measurementId: "G-GSGB2MVBZX"
+};
---
+++ rontra/assets/js/firebase-init.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-storage.js";
+import { firebaseConfig } from "./firebase-config.js";
+
+const app = initializeApp(firebaseConfig);
+try { getAnalytics(app); } catch (e) { /* ignore in non-https/local env */ }
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { app, auth, db, storage, googleProvider };
